3c1087fb4c1d6dec6ce8524fa2c00e52a9dd024e
Marcel Cohen <dmcohen@amazon.com>
2022-07-20 12:15:50 +0100

    Bug fixing, adding descriptive responses and adding capability for new numbers

--- index.js
+++ index.js
@@ -19,38 +19,68 @@ client.on('ready', () => {
     startServer();
 });
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 function startServer() {
     const app = express();
     app.use(express.json());
 
     app.post('/share', (req, res) => {
-        console.log(req.body);
         const payload = req.body;
         
-        client.getChats().then(chats => {
-            const chat =  getReceiverChat(chats, payload);
-            client.sendMessage(chat.id._serialized, payload.message);
-        });
+        client.getChats()
+        .then(chats => {
 
-        res.json(req.body);
+            const chatId =  getReceiverChat(chats, payload);
+            client.sendMessage(chatId, payload.message)
+            .then(message => successResponse(payload, res, message))
+            .catch(error => {
+                console.log("ERROR SENDING MESSAGE")
+                errorResponse(payload, res, error)});
+
+        })
+        .catch(error => {
+            console.log("ERROR GETTING CHATS ");
+            console.log(error);
+            errorResponse(payload, res, error)});
     });
 
     app.listen(port, () => {
-        console.log('SERVER STARTED');
+        console.log(`SERVER STARTED ON PORT ${port}`);
     });
 }
 
 function getReceiverChat(chats, payload) {
+    let chatId;
+    let chat;
     if(payload.receiverType === 'GROUP') {
-        return chats.find(chat => chat.isGroup && chat.name === payload.receiver);
+        chat = chats.find(chat => chat.isGroup && chat.name === payload.receiver);
     } else if (payload.receiverType === 'CONTACT') {
-        return chats.find(chat => !chat.isGroup && chat.name === payload.receiver);
+        chat = chats.find(chat => !chat.isGroup && chat.name === payload.receiver);
     } else {
         const phoneNumber = getSanitizedPhoneNumber(payload.receiver);
-        return chats.find(chat => !chat.isGroup && chat.id.user === phoneNumber);
+        chat = chats.find(chat => !chat.isGroup && chat.id.user === phoneNumber);
     }
+
+    if(chat) {
+        chatId = chat.id._serialized;
+    }
+
+    if(!chatId) {
+        let newPhoneNumber = payload.receiver;
+
+        if(newPhoneNumber.charAt(0) === '+') {
+            newPhoneNumber = payload.receiver.substring(1)
+        }
+
+        newPhoneNumber = getSanitizedPhoneNumber(payload.receiver);
+
+        if(!isNaN(newPhoneNumber)) {
+            chatId = newPhoneNumber + "@c.us";
+        }
+    }
+
+    return chatId;
 }
 
 function getSanitizedPhoneNumber(receiver) {
@@ -58,6 +88,15 @@ function getSanitizedPhoneNumber(receiver) {
         receiver = receiver.substr(1);
     }
     return receiver.replace(' ', '').replace('-', '');
+}
+
+function successResponse(payload, res, message) {
+    res.status(201).json({ message: `Message successfully sent to ${payload.receiver}`, internalResponse: message });
+
+}
+
+function errorResponse(payload, res, error) {
+    res.status(400).json({ error: `Error while sending message to receiver ${payload.receiver}`, internalError: error });
 }
 
 client.initialize();
